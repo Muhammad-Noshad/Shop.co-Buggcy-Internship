b9ab2c98a7f081a232079d09b8fbac685c91a7d5
Muhammad Noshad <muhammadnoshad137@gmail.com>
2024-07-08 17:52:48 +0500

    Updated API config

--- src/hooks/useAPI.js
+++ src/hooks/useAPI.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://shop-co-backend.vercel.app"
+  baseURL: "https://shop-co-backend.vercel.app",
+  withCredentials: true,
 });
 
 export default API;
